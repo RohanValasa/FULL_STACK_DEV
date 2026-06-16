1de63bc1d5ab573b7737215a330fcee458c0c1bb
RohanValasa <rohan.valasa06@gmail.com>
2026-06-16 14:33:31 +0530

    modified files

--- FSD13/streamodule.js
+++ FSD13/streamodule.js
@@ -1,16 +1,8 @@
 const fs = require('fs')
-
-const readableStream = fs.createReadStream('rohan.txt','utf-8')
-const writableStream = fs.createWriteStream('tarun.txt')
-
-readableStream.pipe(writableStream)
-
-writableStream.on('finish', () => {
-  console.log('File copy completed!');
+const readStream = fs.createReadStream('big.txt','utf-8')
+const writeStream = fs.createWriteStream('backup.txt')
+readStream.pipe(writeStream) //file copying
+readStream.on("data", (chunk) => {
+  console.log(chunk);
 });
-
-let x = fs.createReadStream('rohan.txt','utf-8');
-x.on('data',(chunk)=>{
-    console.log(chunk)
-})
 
